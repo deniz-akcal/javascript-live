45a7a7a50f926b268f64e19ccfe7c6433e190b79
Deniz Akcal <deniz.akcal.1@outlook.com>
2026-08-25 16:34:34 -0400

    Did #3 of strings.js.

--- strings.js
+++ strings.js
@@ -15,6 +15,7 @@ sentence = sentence.substring(0,5) + 'I' + sentence.substring(6,24) + 'I ' + sen
 
 console.log(sentence)
 
+console.log("========================================================")
 
 //2. The history of JavaScript is interesting but how many times does the letter a appear? Find a way to count both upper and lower case A's
 
@@ -32,7 +33,39 @@ console.log(count);
 //console.log(current_character.toLowerCase())
 //console.log(jsHistory.length)
 
+console.log("========================================================")
+
 //3. Given the following sentence: Uppercase the entire word scream each time it appears in the sentence
 //I SCREAM, you SCREAM, we all SCREAM for icecream
 
 var text = 'I scream, you scream, we all scream for icecream';
+
+var screamCounter = 0;
+
+const scream_length = "scream".length;
+
+var i = 0;
+
+// console.log("text.length: " + text.length);
+// console.log("scream_length: " + scream_length);
+
+//var screamWordStartIndices = [];
+
+while(i < (text.length-scream_length) ) {
+
+  // console.log("i: " + i);
+  // console.log("text.substring(i,i+scream_length).toLowerCase(): " + text.substring(i,i+scream_length).toLowerCase());
+  if( text.substring(i,i+scream_length).toLowerCase() === "scream" ) {
+    // screamWordStartIndices.push(i);
+    text = text.substring(0,i) + "SCREAM " + text.substring(i+2+scream_length,text.length);
+    i += scream_length;
+  } else {
+    i++;
+  }
+
+}
+
+
+//console.log("screamWordStartIndices: " + screamWordStartIndices);
+
+console.log("text: " + text);
