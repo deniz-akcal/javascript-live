4351e928382e2ad465f90c54f960feacb328a9bd
Deniz Akcal <deniz.akcal.1@outlook.com>
2026-08-25 15:46:25 -0400

    Did #1 of numbers.js.

--- numbers.js
+++ numbers.js
@@ -4,6 +4,8 @@ var num10 = 10;
 var string8 = '8';
 var one = 1;
 
+const number_base = 10;
+console.log(num10 + Number.parseInt(string8,number_base) + one)
 
 // 2. Write a loop that will log only numbers divisible by 3 between 20 - 100
 
